c7d3ad535cfa50e21a398789bfd791986f67eb50
Irara <irara.chen@token.im>
2024-08-07 17:49:22 +0800

    feat: Initial Foundry project

---
+++ src/artifact/Artifact.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ENSRegistry} from "@ens-contracts/contracts/registry/ENSRegistry.sol";
+import {OffchainResolver} from "@offchain-resolver/packages/contracts/contracts/OffchainResolver.sol";
